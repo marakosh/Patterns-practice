23e1073556be889c4f990f6b3d55047e33a4fdf7
marakosh <marat.garifulin160@gmail.com>
2022-12-13 00:22:43 +0400

    Facade

---
+++ Facade/Facade/Program.cs
@@ -0,0 +1,51 @@
+﻿public class MessageSender
+{
+    public void PrintMessage(int wait)
+    {
+        Console.WriteLine($"You have to wait{wait} days");
+        
+        if (wait == 0)
+        {
+            Console.WriteLine("Your order has arrived, you can pick it up!");
+        }
+    }
+}
+
+public class Order
+{
+    public bool Condition { get; set; } = true;
+}
+
+public class Facade
+{
+    MessageSender? sender;
+    Order? order;
+    int wait = 10;
+    public Facade(Order? order, int wait)
+    {
+        this.order = order;
+        this.wait = wait;
+    }
+
+
+    public void StatusQuestion()
+    {
+        Console.WriteLine("Have you placed an order?");
+        order.Condition = Convert.ToBoolean(Console.ReadLine());
+    }
+
+    public void Reguest()
+    {
+        while (true)
+        if (order?.Condition == false)
+        {
+            sender?.PrintMessage(wait);
+            wait--;
+        }
+        else if (order?.Condition == true)
+        {
+                sender?.PrintMessage(wait);
+                break;
+        }
+    }
+}
