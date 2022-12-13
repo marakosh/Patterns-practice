a0026bd908d29ba4ca27e6ec639897a53b99ff55
marakosh <marat.garifulin160@gmail.com>
2022-12-13 15:28:48 +0400

    Proxy

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,37 @@
+﻿
+public interface IPaymentService
+{
+    public void Pay(int amount) { }
+}
+
+public class PaymentService : IPaymentService
+{
+    public void Pay(int amount)
+    {
+        Console.WriteLine($"You paid {amount}");
+    }
+}
+
+public class Proxy : IPaymentService
+{
+    private PaymentService paymentService = new();
+    public Proxy(int amount, bool result)
+    {
+        paymentService = new();
+        Amount = amount;
+        Result = result;
+    }
+    public int Amount { get; set; }
+    public bool Result { get; set; }
+    public void Pay(int amount)
+    {
+        Console.WriteLine($"You paid {amount}");
+    }
+    public void CheckAcces(bool result)
+    {
+        if (result)
+        {
+            paymentService.Pay(Amount);
+        }
+    }
+}
